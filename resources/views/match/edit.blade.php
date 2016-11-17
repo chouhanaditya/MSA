49a3f0340ca27244ddb6cabfaff3558ca62589ff
Aditya Chouhan <achouhan@unomaha.edu>
2016-11-16 20:04:54 -0600

    troubleshooting

--- resources/views/match/edit.blade.php
+++ resources/views/match/edit.blade.php
@@ -1,37 +1,38 @@
 @extends('layouts.app')
 @section('content')
     
-    <h3>Update Match Details</h3>
+    <h3>Update <?php echo ($match['match_name']); ?> Details</h3>
+    <br>
 
     {!! Form::model($match,['method' => 'PATCH','route'=>['match.update',$match->id]]) !!}
 
     <div class="form-group">
-        {!! Form::label('match_start_time', 'Match Start Time:') !!}
+        <h4> Match Start Time:</h4>
         {!! Form::text('match_start_time',null,['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('match_end_time', 'Match End Time:') !!}
+            <h4> Match End Time:</h4>
         {!! Form::text('match_end_time',null,['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('match_team1_score', 'Team 1 Score:') !!}
+    <h4> Team <?php echo ($match['match_team1_id']); ?> Score:</h4>
         {!! Form::text('match_team1_score',null,['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('match_team2_score', 'Team 2 Score:') !!}
+        <h4> Team <?php echo ($match['match_team2_id']); ?> Score:</h4>
         {!! Form::text('match_team2_score',null,['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('match_team1_goals', 'Team 1 Goals Details:') !!}
-        {!! Form::text('match_team1_goals',null,['class'=>'form-control']) !!}
+        <h4> Team <?php echo ($match['match_team1_id']); ?> Goal Details:</h4>
+         {!! Form::text('match_team1_goals',null,['class'=>'form-control']) !!}
     </div>
 
     <div class="form-group">
-        {!! Form::label('match_team2_goals', 'Team 2 Goals Details:') !!}
+        <h4> Team <?php echo ($match['match_team2_id']); ?> Goal Details:</h4>
         {!! Form::text('match_team2_goals',null,['class'=>'form-control']) !!}
     </div>
 
